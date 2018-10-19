678392bdd061853880448aa6653341e974dfd9eb
ryanwevans <ryanwevans@icloud.com>
2018-10-18 17:09:08 -0700

    add #new, #create, #destroy to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+
+  def new
+    @session = Session.new
+  end
+
+  def create
+    @counselor = CampCounselor.find(params[:counselor][:name])
+    @counselor.try(:authenticate, params[:password])
+    if @counselor
+      session[:user_id] = @counselor.id
+      redirect_to camp_counselor_path(@counselor)
+    else
+      redirect_to signin_path
+  end
+
+  def destroy
+    session.delete :user_id
+    redirect_to '/'
+  end
 end
