393a6a59f30ef335b638e6691045d6dfa1818110
ryanwevans <ryanwevans@icloud.com>
2018-10-18 17:37:43 -0700

    add private methods; fill in #new

--- app/controllers/camp_counselors_controller.rb
+++ app/controllers/camp_counselors_controller.rb
@@ -1,15 +1,15 @@
 class CampCounselorsController < ApplicationController
-  
-  def new
+  before_action :set_counselor, only: [:show, :edit, :update, :destroy]
 
+  def new
+    @counselor = CampCounselor.new
   end
 
   def create
-
+    @counselor = CampCounselor.create(counselor_params)
   end
 
   def show
-
   end
 
   def edit
@@ -22,5 +22,16 @@ class CampCounselorsController < ApplicationController
 
   def destroy
 
+  end
+
+
+  private
+
+  def set_counselor
+    @counselor = CampCounselor.find_by(id: params[:id])
+  end
+
+  def counselor_params
+    params.require(:counselor).permit(:name, :password)
   end
 end
