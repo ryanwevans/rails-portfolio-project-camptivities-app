fadfcc1d6cfc4b4736379c7f9033b696de6c2430
ryanwevans <ryanwevans@icloud.com>
2018-10-21 15:47:45 -0700

    add empty CRUD and private methods

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,2 +1,41 @@
 class AssignmentsController < ApplicationController
+  before_action :set_assignment, only: [:show, :edit, :update, :destroy]
+
+  def index
+
+  end
+
+  def show
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+
+  private
+
+  def set_assignment
+
+  end
+
+  def assignment_params
+
+  end
+
 end
