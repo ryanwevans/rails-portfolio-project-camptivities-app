16555776a24d973e2e646fc00d72a62405c21318
ryanwevans <ryanwevans@icloud.com>
2018-10-21 16:26:05 -0700

    add root to routes.rb; add #require_log_in, #logged_in?, #current_counselor to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :current_counselor
+  before_action :require_log_in, except: [:new, :create, :home]
+
+  helper_method :current_counselor
+
+   def logged_in?
+     !!current_counselor
+   end
+
+
+ private
+
+   def require_log_in
+     redirect_to root_path unless logged_in?
+   end
+
+   def current_counselor
+     @current_counselor ||= CampCounselor.find(session[:user_id]) if session[:user_id]
+   end
+
 end
