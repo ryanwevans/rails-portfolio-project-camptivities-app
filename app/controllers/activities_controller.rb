23d7495c8448600c38d738997ca881a30f8fde1e
ryanwevans <ryanwevans@icloud.com>
2018-10-21 15:42:53 -0700

    complete CRUD and private methods

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,40 +2,50 @@ class ActivitiesController < ApplicationController
   before_action :set_activity, only: [:show, :edit, :update, :destroy]
 
   def index
-
+    @activities = Activity.all
   end
 
   def show
   end
 
   def new
-
+    @activity = Activity.new
   end
 
   def create
-
+    @activity = Activity.create(activity_params)
+    if @activity.save
+      redirect_to activity_path(@activity)
+    else
+      redirect_to :new
+    end
   end
 
   def edit
   end
 
   def update
-
+    if @activity.update(activity_params)
+      redirect_to @activity
+    else
+      redirect_to :edit
+    end
   end
 
   def destroy
-
+    @activity.destroy
+    redirect_to activities_path
   end
 
 
   private
 
   def set_activity
-
+    @activity = Activity.find_by(id: params[:id])
   end
 
   def activity_params
-
+    params.require(:activity).permit(:name)
   end
 
 end
