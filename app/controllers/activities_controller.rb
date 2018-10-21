525aef3a8973a2b98fa8650cccb1a695b5c9e7a1
ryanwevans <ryanwevans@icloud.com>
2018-10-21 15:35:38 -0700

    add empty CRUD and private methods

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,41 @@
 class ActivitiesController < ApplicationController
+  before_action :set_activity, only: [:show, :edit, :update, :destroy]
+
+  def index
+
+  end
+
+  def show
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+
+  private
+
+  def set_activity
+
+  end
+
+  def activity_params
+
+  end
+
 end
