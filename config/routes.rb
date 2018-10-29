90b49ec8d0cda380f56aa92baa96ce2287355078
ryanwevans <ryanwevans@icloud.com>
2018-10-28 18:18:37 -0700

    removed erroneous commas in nested routes; add header to sessions#new

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'sessions#new'
 
-  resources :camp_counselors, do
+  resources :camp_counselors do
     resources :assignments, only: [:show, :index]
   end
-  resources :activities, do
+  resources :activities do
     resources :assignments, only: [:index, :edit]
   end
   resources :assignments
