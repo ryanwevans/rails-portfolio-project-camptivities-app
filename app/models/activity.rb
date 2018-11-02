c8b4dc8f1f1e9b5b9ba775e30f394d6bb1691797
ryanwevans <ryanwevans@icloud.com>
2018-11-01 17:19:15 -0700

    remove #counselors_assigned and #num_of_openings, not needed

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -5,11 +5,4 @@ class Activity < ApplicationRecord
   has_many :assignments
   has_many :camp_counselors, through: :assignments
 
-  def counselors_assigned
-    Assignment.where((activity_id: self.id) && (camp_counselor_id: !nil)).count
-  end
-
-  def num_of_openings
-    self.counselors_needed-self.counselors_assigned
-  end
 end
