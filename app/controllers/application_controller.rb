84ce16c737322e67e3ea32b09dc4a52c39d3228e
ryanwevans <ryanwevans@icloud.com>
2018-11-08 17:22:39 -0800

    remove :before_action :current_user, not needed; remove email feild in Sessions#new view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :current_user
   before_action :require_log_in
 
   helper_method :current_user
@@ -18,7 +17,7 @@ class ApplicationController < ActionController::Base
    end
 
    def current_user
-     @current_user ||= CampCounselor.find(session[:user_id]) if session[:user_id]
+     @current_user ||= CampCounselor.find_or_create_by(id: session[:user_id])
    end
 
 end
