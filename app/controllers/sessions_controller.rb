5a67fdd959b8e4cebfd1ea3383ea5ed5083d0649
ryanwevans <ryanwevans@icloud.com>
2018-11-22 16:49:09 -0800

    fixed password validation for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,19 +5,12 @@ class SessionsController < ApplicationController
     @counselor = CampCounselor.new
   end
 
-  # def create
-  #   return head(:forbidden) unless counselor = CampCounselor.find_by(name: params[:camp_counselor][:name]).try(:authenticate, params[:camp_counselor][:password])
-  #   session[:user_id] = counselor.id
-  #   redirect_to camp_counselor_path(counselor)
-  # end
-
   def create
-    counselor = CampCounselor.find_by(name: params[:camp_counselor][:name])
-    if counselor.try(:authenticate, params[:password])
+    if counselor = CampCounselor.find_by(name: params[:camp_counselor][:name]).try(:authenticate,         params[:camp_counselor][:password])
       session[:user_id] = counselor.id
       redirect_to camp_counselor_path(counselor)
     else
-      flash[:notice] = "Incorrect Name or Password!"
+      flash[:notice] = "Incorrect Name or Password"
       redirect_to signin_path
     end
   end
