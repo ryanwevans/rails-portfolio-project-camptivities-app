f9518a33e0c567b3060f1ae05722c42fe3e8d94c
ryanwevans <ryanwevans@icloud.com>
2018-12-13 20:23:08 -0800

    move admin links to activities_helper

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,2 +1,7 @@
 module ActivitiesHelper
+  def activity_admin_links
+    if @current_user.admin
+       link_to "Delete Activity", @activity, method: :delete, data: { confirm: "Delete This Activity?" }
+    end
+  end
 end
