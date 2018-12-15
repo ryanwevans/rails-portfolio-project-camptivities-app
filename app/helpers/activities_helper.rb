f527292769cdca6854c37647d260a1fa853da21e
ryanwevans <ryanwevans@icloud.com>
2018-12-14 23:09:45 -0800

    move #assignment_availability from activities_helper to a partial; render partial in show view

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -11,23 +11,4 @@ module ActivitiesHelper
     end
   end
 
-  def activity_assignment_availability
-    if @assignment.filled
-      "One assignment is full"
-    else
-      "This assignment is still open"
-      form_for @assignment do |f|
-      f.hidden_field :camp_counselor_id, :value => current_user.id
-      f.hidden_field :filled, :value => true
-      f.submit "Signup to Oversee This Activity!"
-      end
-    end
-  end
-  
 end
-
-# if !assignment.filled
-#   link_to "Sign Up For This Assignment", assignment_path(assignment)
-# else
-#   "One Assignment is Full"
-# end
