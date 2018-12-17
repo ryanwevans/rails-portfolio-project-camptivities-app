2ec3dc83856620565ac9faea6bf305328d4e238a
ryanwevans <ryanwevans@icloud.com>
2018-12-16 17:09:38 -0800

    fix omniauth issue with has_secure_password; on omniauth login redirect to CampCounselor#edit page to add missing counselor info

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,15 +10,13 @@ class SessionsController < ApplicationController
       counselor = CampCounselor.find_by(name: params[:camp_counselor][:name]).try(:authenticate, params[:camp_counselor][:password])
       session[:user_id] = counselor.id
       redirect_to camp_counselor_path(counselor)
-
-    elsif counselor = CampCounselor.find_or_create_by(uid: auth['uid']) do |u|
+    elsif counselor = CampCounselor.find_or_create_by!(uid: auth['uid']) do |u|
       u.name = auth['info']['name']
       u.email = auth['info']['email']
-      u.uid = auth['info']['uid']
+      u.password_digest = auth['uid']
       end
       session[:user_id] = counselor.id
-      redirect_to camp_counselor_path(counselor)
-
+      redirect_to edit_camp_counselor_path(counselor)
     else
       flash[:notice] = "Please Try Again"
       redirect_to signin_path
--- app/models/camp_counselor.rb
+++ app/models/camp_counselor.rb
@@ -6,7 +6,8 @@ class CampCounselor < ApplicationRecord
   validates_presence_of :email
   validates_uniqueness_of :email
 
-  # validates_presence_of :age
+  validates_presence_of :age
+  validates_presence_of :admin
 
   has_many :assignments
   has_many :activities, through: :assignments
