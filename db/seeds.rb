a02ca752e673a873fb25b18c089c99950ee36751
ryanwevans <ryanwevans@icloud.com>
2019-01-06 19:00:44 -0800

    add camp data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,15 @@
 
 
 DATA = {
+  :camp_keys =>
+    ["name"],
+  :camps => [
+    ["Camp Chautaugua"],
+    ["Camp Adirondack"],
+    ["Mt. Baker Camp"],
+    ["Camp Snoqualmie"],
+    ["Camp Boundary Waters"],
+  ],
   :camp_counselor_keys =>
     ["name", "age", "admin", "password"],
   :camp_counselors => [
@@ -24,27 +33,37 @@ DATA = {
     ["Winston Wallis", 13, false, "password"]
   ],
   :activity_keys =>
-   ["name", "description"],
+   ["name", "description", "camp_id"],
   :activities => [
-    ["Bike Riding", "Feel the wind in your hair! This ride will take you through scenic vistas!"],
-    ["Nature Hike", "Get your exercise while winding through country trails along rivers and lakes."],
-    ["Canoeing", "Paddle along the calm shores of a beautiful lake."],
-    ["Soccer", "Feed your competitive side! Run and kick the ball while having fun with your fellow campers!"],
-    ["Swimming", "This isn't just swimming! We have diving platforms and fun toys to play with in the lake!"],
-    ["Go Carts", "Speed along the track and race all your friends to the finish line!"],
-    ["Volleyball", "Keep the ball in the air! Join a team or come as a team with your friends to play!"],
-    ["Arts and Crafts", "Are you creative and artistic? Even if you answered no, this is your opportunity to try and have fun with several different arts and crafts!"],
-    ["Trail Run", "Single track, lots of elevation and the amazing views are the payoff!"],
-    ["Archery", "Test your accuracy with a bow and arrow!"],
-    ["Baking", "Practice your favorite recipes and share them with your fellow campers!"],
-    ["Skateboarding", "These aren't your average skateboards! These are off-road boards that we'll ride down hills!"],
-    ["Waterskiing", "Glide atop the calm waters of the lake at breathtaking speeds! This activity is open to beginners, so come give it a try! You are guaranteed to have a splashing good time!"]
+    ["Bike Riding", "Feel the wind in your hair! This ride will take you through scenic vistas!", 1],
+    ["Nature Hike", "Get your exercise while winding through country trails along rivers and lakes.", 2],
+    ["Canoeing", "Paddle along the calm shores of a beautiful lake.", 3],
+    ["Soccer", "Feed your competitive side! Run and kick the ball while having fun with your fellow campers!", 1],
+    ["Swimming", "This isn't just swimming! We have diving platforms and fun toys to play with in the lake!", 5],
+    ["Go Carts", "Speed along the track and race all your friends to the finish line!", 1],
+    ["Volleyball", "Keep the ball in the air! Join a team or come as a team with your friends to play!", 1],
+    ["Arts and Crafts", "Are you creative and artistic? Even if you answered no, this is your opportunity to try and have fun with several different arts and crafts!", 4],
+    ["Trail Run", "Single track, lots of elevation and the amazing views are the payoff!", 4],
+    ["Archery", "Test your accuracy with a bow and arrow!", 5],
+    ["Baking", "Practice your favorite recipes and share them with your fellow campers!", 2],
+    ["Skateboarding", "These aren't your average skateboards! These are off-road boards that we'll ride down hills!", 2],
+    ["Waterskiing", "Glide atop the calm waters of the lake at breathtaking speeds! This activity is open to beginners, so come give it a try! You are guaranteed to have a splashing good time!", 3]
   ]
 }
 
 def main
   make_camp_counselors
   make_activities
+end
+
+def make_camps
+  DATA[:camps].each do |camp|
+    new_camp = Camp.new
+    camp.each_with_index do |attribute, i|
+      new_camp.send(DATA[:camp_keys][i]+"=", attribute)
+    end
+    new_camp.save
+  end
 end
 
 def make_camp_counselors
