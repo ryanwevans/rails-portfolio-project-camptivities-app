9f85af23f174c546da068a60a5846051bcfa2c03
ryanwevans <ryanwevans@icloud.com>
2019-01-09 18:08:23 -0800

    create #index, #show, #new, #create, #edit, #update; add camp#new view with partial

--- app/controllers/camps_controller.rb
+++ app/controllers/camps_controller.rb
@@ -1,6 +1,44 @@
 class CampsController < ApplicationController
   before_action :set_camp, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @camps = Camp.all
+  end
+
+  def show
+    @assignment = @activity.assignments.where("camp_counselor_id=?", @current_user.id)
+    @assignments = @activity.assignments.order(:filled)
+  end
+
+  def new
+    @camp = Camp.new
+  end
+
+  def create
+    @camp = Camp.create(camp_params)
+    if @camp.save
+      redirect_to camp_path(@camp)
+    else
+      if @camp.name==""
+        flash[:notice] = "Invalid Entry - Name Cannot Be Blank"
+      end
+      render action: :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @camp.update(camp_params)
+      redirect_to @camp
+    else
+      if @camp.name==""
+        flash[:notice] = "Camp name is required"
+      end
+      redirect_to :edit
+    end
+  end
 
   private
 
@@ -9,7 +47,7 @@ class CampsController < ApplicationController
   end
 
   def camp_params
-    params.require(:camp).permit(:name)
+    params.require(:camp).permit(:name, activities_attributes: [:id, :name, :description, :camp_id])
   end
 
 end
