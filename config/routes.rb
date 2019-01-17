05bd036e3a22a4a6340864698aeb270cb44a5231
ryanwevans <ryanwevans@icloud.com>
2019-01-16 19:03:55 -0800

    update routes.rb for new nested resources; remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'sessions#new'
 
-  resources :camp_counselors do
-    resources :assignments, only: [:show, :index]
-    resources :comments, shallow: true
+  resources :camps do
+    resources :activities
   end
 
   resources :activities do
@@ -12,9 +11,13 @@ Rails.application.routes.draw do
     resources :comments, shallow: true
   end
 
-  resources :camps
-
   resources :assignments
+
+  resources :camp_counselors do
+    resources :assignments, only: [:show, :index]
+    resources :comments, shallow: true
+  end
+
 
   get '/auth/facebook/callback' => 'sessions#create'
 
