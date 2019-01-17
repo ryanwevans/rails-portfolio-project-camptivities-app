763f455c33a81eaa25bb64985f8c40df3416aedb
ryanwevans <ryanwevans@icloud.com>
2019-01-16 19:06:23 -0800

    remove unused relationships

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,20 +4,11 @@ class Activity < ApplicationRecord
 
   validates_presence_of :description
 
-  # has_many :assignments
-  # has_many :camp_counselors, through: :assignments
-  #
-  # has_many :comments
-  # has_many :camp_counselors, through: :comments
-
-  # belongs_to :camp
-
-
-  belongs_to :camp
+  has_many :comments
 
   has_many :assignments
   has_many :camp_counselors, through: :assignments
 
-  has_many :comments
+  belongs_to :camp
 
 end
