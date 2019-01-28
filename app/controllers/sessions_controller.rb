d72992fa7745629978f891e7b6ebfb8c7c2b8be9
ryanwevans <ryanwevans@icloud.com>
2019-01-27 18:05:23 -0800

    move #create logic to SessionsHelper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,30 +1,13 @@
 class SessionsController < ApplicationController
   skip_before_action :require_log_in, only: [:new, :create]
+  include SessionsHelper
 
   def new
     @counselor = CampCounselor.new
   end
 
   def create
-    if params[:camp_counselor]
-      if counselor = CampCounselor.find_by(name: params[:camp_counselor][:name]).try(:authenticate, params[:camp_counselor][:password])
-        session[:user_id] = counselor.id
-        redirect_to camp_counselor_path(counselor)
-      else
-        flash[:notice] = "Please Try Again"
-        redirect_to signin_path
-      end
-    elsif counselor = CampCounselor.find_or_create_by!(uid: auth['uid']) do |u|
-      u.name = auth['info']['name']
-      u.email = auth['info']['email']
-      u.password_digest = auth['uid']
-      end
-      session[:user_id] = counselor.id
-      redirect_to camp_counselor_path(counselor)
-    else
-      flash[:notice] = "Please Try Again"
-      redirect_to signin_path
-    end
+    sign_in_logic
   end
 
   def destroy
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,25 @@
 module SessionsHelper
+
+  def sign_in_logic
+    if params[:camp_counselor]
+      if counselor = CampCounselor.find_by(name: params[:camp_counselor][:name]).try(:authenticate, params[:camp_counselor][:password])
+        session[:user_id] = counselor.id
+        redirect_to camp_counselor_path(counselor)
+      else
+        flash[:notice] = "Please Try Again"
+        redirect_to signin_path
+      end
+    elsif counselor = CampCounselor.find_or_create_by!(uid: auth['uid']) do |u|
+      u.name = auth['info']['name']
+      u.email = auth['info']['email']
+      u.password_digest = auth['uid']
+      end
+      session[:user_id] = counselor.id
+      redirect_to camp_counselor_path(counselor)
+    else
+      flash[:notice] = "Please Try Again"
+      redirect_to signin_path
+    end
+  end
+
 end
