34c98b5d7b365e92ea9ebc6a15c54405c4cbf103
ryanwevans <ryanwevans@icloud.com>
2019-02-05 17:33:40 -0800

    remove unneeded routes and methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,4 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
-
-  def index
-    if params[:activity_id]
-      @activity = Activity.find_by(id: params[:activity_id])
-      @comments = @activity.comments
-    else
-      @comments = Comment.all
-    end
-  end
-
-  def show
-  end
 
   def new
     @comment = Comment.new
@@ -27,33 +14,10 @@ class CommentsController < ApplicationController
     end
   end
 
-  def edit
-  end
-
-  def update
-    if @comment.update(comment_params)
-      redirect_to @comment
-    else
-      if @comment.title==""
-        flash[:notice] = "Title is required"
-      else @comment.content==""
-        flash[:notice] = "Content is required"
-      end
-      render edit_comment_path
-    end
-  end
-
-  def destroy
-    @comment.destroy
-    redirect_to activities_path
-  end
 
 
   private
 
-  def set_comment
-    @comment = Comment.find_by(id: params[:id])
-  end
 
   def comment_params
     params.require(:comment).permit(:title, :content, :camp_counselor_id, :activity_id)
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     resources :assignments
   end
 
-  resources :comments 
+  resources :comments , only: [:new, :create]
 
   resources :assignments
 
