beb322b9c0af136e909d1368d137f05b8cd9601e
ryanwevans <ryanwevans@icloud.com>
2019-02-14 22:27:21 -0800

    fixed email, age keys mismatch order with data

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,13 +14,13 @@ DATA = {
     ["name", "location", "description"],
   :camps => [
     ["Camp Chautaugua", "Upstate New York", "Exclusive 1950's era family resort for those with high-end tastes"],
-    ["Camp Adirondack", "New York State", "Nestled on a tranquil lake in the Adirondack Mountains of upstate New York"],
+    ["Camp Adirondack", "Upstate New York", "Nestled on a tranquil lake in the Adirondack Mountains of upstate New York"],
     ["Mt. Baker Camp", "Mt. Baker Ski Area", "When the ski season is over, we have endless hiking trails, climbing, and star-gazing"],
     ["Camp Snoqualmie", "Glacier, Washington", "With several lakes in the area, we focus on water sports and activities"],
     ["Camp Boundary Waters", "Northern Minnesota", "In the land of ten thousand lakes, we have so many that we spend days canoeing and kayaking and nights camping under the clear Minnesota skies"],
   ],
   :camp_counselor_keys =>
-    ["name", "age", "email", "admin", "password"],
+    ["name", "email", "age", "admin", "password"],
   :camp_counselors => [
     ["Max Charles", "max@max.com", 32, true, "password"],
     ["Skai Jackson", "skai@skai.com", 10, false, "password"],
@@ -48,6 +48,23 @@ DATA = {
     ["Baking", "Practice your favorite recipes and share them with your fellow campers!", 2],
     ["Skateboarding", "These aren't your average skateboards! These are off-road boards that we'll ride down hills!", 2],
     ["Waterskiing", "Glide atop the calm waters of the lake at breathtaking speeds! This activity is open to beginners, so come give it a try! You are guaranteed to have a splashing good time!", 3]
+  ],
+  :comment_keys =>
+   ["title", "content", "camp_counselor_id", "activity_id"],
+  :comments => [
+    ["Awesome!", "Cupcake ipsum dolor sit amet candy canes jelly beans. Cotton candy tootsie roll cheesecake sesame snaps ice cream jelly sesame snaps. Jujubes gummies apple pie jelly beans jujubes donut soufflé.", 1, 13],
+    ["So cool...", "Cupcake ipsum dolor sit. Amet chupa chups muffin apple pie sweet pudding candy canes gingerbread. Sweet roll chocolate apple pie toffee gingerbread.", 2, 12],
+    ["Supreme!", "Cupcake ipsum dolor sit. Amet candy canes lemon drops chupa chups caramels caramels soufflé. Sweet cheesecake cupcake. Soufflé chupa chups wafer bear claw biscuit dragée marzipan.", 3, 11],
+    ["So. Much. Fun.", "Cupcake ipsum dolor sit amet lollipop soufflé muffin. Icing chocolate cake bear claw. Carrot cake powder bear claw marzipan cake jelly beans cotton candy lollipop candy. Candy canes chocolate cake cotton candy cookie gummi bears caramels brownie croissant.", 4, 10],
+    ["Amazing", "Cupcake ipsum dolor sit amet pastry danish carrot cake. Cake pudding donut toffee muffin jelly. Liquorice gummies sweet carrot cake tart sesame snaps powder pie", 5, 9],
+    ["Great Time!", "Cupcake ipsum dolor sit. Amet jelly beans donut biscuit cake bear claw pudding cookie. Gingerbread jelly-o jelly candy marshmallow croissant lollipop pudding!", 6, 8],
+    ["Super Cool", "Cupcake ipsum dolor. Sit amet cotton candy fruitcake. Apple pie oat cake halvah halvah muffin candy canes muffin icing. Brownie bonbon soufflé jelly-o caramels wafer bear claw tart sweet roll.", 7, 7],
+    ["Good Time", "Cupcake ipsum dolor sit amet chupa chups chocolate cake. Soufflé dessert gummies. Pie tart wafer oat cake!", 8, 6],
+    ["Neato", "Cupcake ipsum dolor sit amet toffee bear claw topping. Cotton candy tiramisu halvah topping donut soufflé. Halvah oat cake jelly chocolate cake brownie!", 9, 5],
+    ["Great Day", "Cupcake ipsum dolor sit. Amet pastry carrot cake fruitcake. Muffin bonbon cake caramels jujubes tootsie roll chocolate cake. Sugar plum jujubes toffee tiramisu macaroon apple pie lollipop biscuit candy canes!", 1, 4],
+    ["Awesome Activity", "Cupcake ipsum dolor sit. Amet biscuit powder jelly beans. Chupa chups halvah chocolate tootsie roll lemon drops pastry jujubes. Pudding pastry marshmallow cotton candy topping oat cake cheesecake!", 2, 3],
+    ["Really Neat", "Cupcake ipsum dolor sit amet wafer tiramisu sweet jelly. Chocolate cake toffee chocolate bar croissant icing toffee candy candy canes. Cheesecake ice cream marzipan chupa chups marzipan wafer macaroon!", 3, 2],
+    ["Super Super!!", "Cupcake ipsum dolor sit. Amet chupa chups candy canes lemon drops caramels dragée sweet powder. Chocolate bar jelly tiramisu gingerbread danish jelly-o. Gummi bears cheesecake dessert!", 4, 1]
   ]
 }
 
@@ -55,6 +72,7 @@ def main
   make_camp_counselors
   make_camps
   make_activities
+  make_comments
 end
 
 def make_camp_counselors
@@ -88,6 +106,16 @@ def make_camps
       new_camp.send(DATA[:camp_keys][i]+"=", attribute)
     end
     new_camp.save
+  end
+end
+
+def make_comments
+  DATA[:comments].each do |comment|
+    new_comment = Comment.new
+    comment.each_with_index do |attribute, i|
+      new_comment.send(DATA[:comment_keys][i]+"=", attribute)
+    end
+    new_comment.save
   end
 end
 
